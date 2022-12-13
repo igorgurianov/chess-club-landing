6676bb86b8236bc51a1a441d2492860c356f6823
Igor Gurianov <igor.gurianov@inbox.ru>
2022-12-13 13:50:35 +0500

    Feature: section Team

---
+++ script.js
@@ -0,0 +1,11 @@
+// Initialize Swiper
+new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // Курсор перетаскивания
+    grabCursor: true,
+    // Переключение при клике на слайд
+    slideToClickedSlide: true,
+});
