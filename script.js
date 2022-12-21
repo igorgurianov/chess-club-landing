00f3610c062cb71917303437b935fa93b10903b5
Igor Gurianov <igor.gurianov@inbox.ru>
2022-12-21 15:05:52 +0500

    Feature: desktop and tablet markup

--- script.js
+++ script.js
@@ -8,4 +8,20 @@ new Swiper('.swiper', {
     grabCursor: true,
     // Переключение при клике на слайд
     slideToClickedSlide: true,
+    // Количество слайдов для показа
+    slidesPerView: 1,
+    // Отступ между слайдами
+
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 28,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 80,
+
+        }
+    }
 });
+new swiperTeam('.swiperttest')
